6b51103aa756775291d1d26e2893b44049b5a5f4
Andrew Yan <Andrew Yan>
2017-01-14 11:39:12 -0800

    Added Documentation for some classes

--- UI/src/main/LoginFail.java
+++ UI/src/main/LoginFail.java
@@ -6,6 +6,11 @@ import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.geometry.*;
 
+/*
+ * Class: LoginFail
+ * This class creates a pop-up presented to the user whenever an action they try to peform fails
+ */
+
 public class LoginFail {
 	
 	public static void display(String title, String message){
